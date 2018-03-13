f8db2ac7c017477dca7af19dc96f4ed4392feb92
github username <github email address>
2018-03-13 09:49:51 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
-class Application < Sinatra::Base
+class App < Sinatra::Base
   # Write your code here!
-
+  get '/' do
+    erb :index
+  end
+  post '/greet' do
+    @var = params[:user_name]
+    erb :greet
+  end
 end
